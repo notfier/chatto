18955598474b4fca14a4c92cc1a7bbe3469946f5
notfier <notfier@gmail.com>
2016-01-16 23:27:14 +0200

    setup frontend develop env

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+render(
+    React.createElement( 'h1', null, 'Hello Chatto World!' ),
+    document.getElementById( 'root' )
+);
