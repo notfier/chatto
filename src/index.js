be33ee068a868d2fadea98d3441b0e2b446aada1
notfier <notfier@gmail.com>
2016-01-30 19:20:05 +0200

    update for react-router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import './styles/styles.styl';
 
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, Link } from 'react-router';
 
 import { Home } from './home';
 
 
 render(
-    <Home />,
+    (
+        <Router history={ browserHistory }>
+            <Route path='/' component={ Home }>
+            </Route>
+        <Router/>
+    ),
     document.getElementById( 'root' )
 );
